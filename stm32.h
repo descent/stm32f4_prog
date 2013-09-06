42ceae02a233f736b43223bd43292c04c31aff04
descent <descent@weiya.com.tw>
2013-09-06 11:55:51 +0800

    add systick_isr(), int_isr(), svc_isr(), pendsv_isr()

--- stm32.h
+++ stm32.h
@@ -1,9 +1,8 @@
 #ifndef STM32_H
 #define STM32_H
-#define GPIOB_CRL     (*(volatile unsigned long *)0x40010c00)
-#define GPIOB_BSRR    (*(volatile unsigned long *)0x40010c10)
-#define GPIOB_BRR     (*(volatile unsigned long *)0x40010c14)
-#define RCC_APB2ENR   (*(volatile unsigned long *)0x40021018)
+
+#include "lib_mygpio_led.h"
+
 #define STACK_SIZE 64
 extern unsigned long _etext;
 extern unsigned long _data;
@@ -24,6 +23,40 @@ void ResetISR(void)
   main();
 }
 
+void pendsv_isr(void)
+{
+  int i=5;
+
+  ++i;
+#if 1
+  //void test_led();
+  //test_led();
+  /* PD12 to be toggled */
+  GPIO_SetBits(GPIOD, GPIO_Pin_12); 
+  /* Insert delay */
+  Delay(0x3FFFFF);
+
+#endif
+}
+
+void svc_isr(void)
+{
+  // pendsv set:
+  //ref: @ Cortex™-M3 Technical Reference Manual (file name: DDI0337E_cortex_m3_r1p1_trm.pdf) p8-19
+  *( unsigned long *) 0xE000ED04 |= 0x10000000;
+  GPIO_SetBits(GPIOD, GPIO_Pin_13); 
+  /* Insert delay */
+  Delay(0x3FFFFF);
+}
+
+void systick_isr(void)
+{
+}
+
+void int_isr(void)
+{
+}
+
 typedef void (*pfnISR)(void);
 __attribute__((section(".stackares")))
 static unsigned long pulStack[STACK_SIZE];
@@ -33,7 +66,21 @@ __attribute__((section(".isr_vector")))
 pfnISR VectorTable[]=
 {
   (pfnISR)((unsigned long)pulStack+sizeof(pulStack)),
-  ResetISR
+  ResetISR, // 1
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  int_isr,
+  svc_isr,    // 11
+  int_isr,
+  int_isr,
+  pendsv_isr, // 14
+  systick_isr // 15
 };
 
 #endif
