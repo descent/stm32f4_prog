a08967a88ebf6784827be425876d60a15a3b81b8
descent <descent0@gmail.om>
2013-09-18 23:31:57 +0800

    add the 3rd process - proc_c

--- process/c/cs.c
+++ process/c/cs.c
@@ -61,7 +61,7 @@ typedef struct Process_
 
 Process *ready_process;
 
-#define NR_NATIVE_PROCS 2
+#define NR_NATIVE_PROCS 3
 #define A_PROC_STACK 0x400
 #define PROC_STACK (A_PROC_STACK*NR_NATIVE_PROCS)
 char proc_stack[PROC_STACK];
@@ -70,7 +70,7 @@ Process user_proc_table[NR_NATIVE_PROCS] =
 {
   {proc_a, 0},
   {proc_b, 0},
-//  {proc_c, 0},
+  {proc_c, 0},
 };
 
 void init_proc(void)
