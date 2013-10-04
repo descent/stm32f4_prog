6d32fd764f96ddf4aac860f43f0a73dab6bbec97
descent <descent@weiya.com.tw>
2013-10-04 11:57:38 +0800

    use another method to implement system call

--- process/cpp/syscall.cpp
+++ process/cpp/syscall.cpp
@@ -1,11 +1,29 @@
 #include "asm_syscall.h"
 
 typedef void* SystemCall;
+typedef int (*FuncPtr)();
+
+struct Arg
+{
+};
 
 int sys_get_ticks()
 {
   extern int ticks;
   return ticks;
+}
+
+// ref: http://www.coactionos.com/embedded-design/133-effective-use-of-arm-cortex-m3-svcall.html
+void service_call(FuncPtr func_ptr, Arg *arg)
+{
+  __asm__ volatile("svc 0");
+}
+
+int get_ticks()
+{
+  Arg *arg=0;
+
+  service_call(sys_get_ticks, arg);
 }
 
 SystemCall sys_call_table[NR_SYS_CALL] = {(void*)sys_get_ticks};
