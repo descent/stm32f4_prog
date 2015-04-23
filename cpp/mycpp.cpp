7a90d6f327e04851c1922960396624f41ffd5a21
descent <descent0@gmail.com>
2015-04-23 14:40:02 +0800

    add some function for using std::vector

--- cpp/mycpp.cpp
+++ cpp/mycpp.cpp
@@ -1,5 +1,7 @@
 #include "stm32.h"
 
+#include <vector>
+
 int print(int i)
 {
   i+=1;
@@ -11,10 +13,80 @@ int print(char c)
 }
 
 
+
+void *dso_handle_;
+void *__dso_handle;
+
+extern "C" void _exit()
+{
+}
+
+char brk_area[1024];
+
+extern "C" char *_sbrk(char *increment)
+{
+  char *ptr = brk_area;
+  return ptr;
+}
+
+extern "C"
+int _kill(int a, int b)
+{
+  return a;
+}
+
+extern "C"
+int _getpid()
+{
+  int i;
+  return i;
+}
+
+extern "C"
+int _write(int fd, const void *buf, int count)
+{
+}
+
+extern "C"
+int open(const char *pathname, int flags, int mode)
+{
+}
+
+extern "C"
+int _isatty(int fd)
+{
+}
+
+
+extern "C"
+int _close(int fd)
+{
+}
+
+extern "C"
+int _fstat(int fd, struct stat *buf)
+{
+}
+
+extern "C"
+int _read(int fd, void *buf, int count)
+{
+}
+
+extern "C"
+int _lseek(int fd, int offset, int whence)
+{
+}
+
+
 void mymain(void)
 {
+#if 0
+  std::vector<int> vec;
+  vec.push_back(1);
   print(35);
   print('A');
   while(1);
+#endif 
 }
 
