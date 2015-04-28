518164e4ca3ea6a974202dfcc221d81aa9869db0
descent <descent0@gmail.com>
2015-04-28 16:53:52 +0800

    using mymalloc for testing

--- cpp/mycpp.cpp
+++ cpp/mycpp.cpp
@@ -1,5 +1,7 @@
 #include "stm32.h"
 
+#include "mem.h"
+
 #include <vector>
 
 int print(int i)
@@ -12,6 +14,9 @@ int print(char c)
  c-=1;
 }
 
+int print(const char *str)
+{
+}
 
 
 void *dso_handle_;
@@ -21,7 +26,7 @@ extern "C" void _exit()
 {
 }
 
-char brk_area[1024];
+char brk_area[10240];
 
 extern "C" char *_sbrk(char *increment)
 {
@@ -78,14 +83,82 @@ int _lseek(int fd, int offset, int whence)
 {
 }
 
+static char memarea[10240];
+
+template <class T>
+class my_allocator
+{
+public:
+  typedef int    size_type;
+  typedef int difference_type;
+  typedef T*        pointer;
+  typedef const T*  const_pointer;
+  typedef T&        reference;
+  typedef const T&  const_reference;
+  typedef T         value_type;
+
+  my_allocator() {}
+  my_allocator(const my_allocator&) {}
+
+
+
+  pointer   allocate(size_type n, const void * = 0) {
+              T* t = (T*) mymalloc(n * sizeof(T));
+              print("used my_allocator to allocate   at address ");
+              print((int)t);
+              print("\r\n");
+
+              return t;
+            }
+  
+  void      deallocate(void* p, size_type) {
+              if (p) {
+                myfree(p);
+                print("used my_allocator to deallocate at address ");
+                print((int)p);
+                print("\r\n");
+              } 
+            }
+
+  pointer           address(reference x) const { return &x; }
+  const_pointer     address(const_reference x) const { return &x; }
+  my_allocator<T>&  operator=(const my_allocator&) { return *this; }
+  void              construct(pointer p, const T& val) 
+                    { new ((T*) p) T(val); }
+  void              destroy(pointer p) { p->~T(); }
+
+  size_type         max_size() const { return int(-1); }
+
+  template <class U>
+  struct rebind { typedef my_allocator<U> other; };
+
+  template <class U>
+  my_allocator(const my_allocator<U>&) {}
+
+  template <class U>
+  my_allocator& operator=(const my_allocator<U>&) { return *this; }
+};
 
 void mymain(void)
 {
-#if 0
-  std::vector<int> vec;
-  vec.push_back(1);
+#if 1
+  char a, b, c , d, e, f;
+  //std::vector<int, __gnu_cxx::new_allocator<int> > vec;
+  std::vector<char, my_allocator<char> > vec;
   print(35);
   print('A');
+  for (int i=0 ; i < 5 ; ++i)
+  {
+    vec.push_back(i);
+  }
+  a = vec[0];
+  b = vec[1];
+  c = vec[2];
+  d = vec[3];
+  e = vec[4];
+
+  char z=a+b+c+d;
+
   while(1);
 #endif 
 }
