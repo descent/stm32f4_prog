6ca40106017e1a012ba38f975f81b07ea5c9cb4f
descent <descent0@gmail.com>
2016-10-13 13:13:10 +0800

    add init_SPI1(), look like spi 1 can work

--- spi/my_spi.c
+++ spi/my_spi.c
@@ -660,6 +660,85 @@ void delay(uint32_t delay )
   while(delay) delay--;
 }
 
+void init_SPI1(void)
+{
+	
+	GPIO_InitTypeDef GPIO_InitStruct;
+	SPI_InitTypeDef SPI_InitStruct;
+	
+	// enable clock for used IO pins
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	
+	/* configure pins used by SPI1
+	 * PA5 = SCK
+	 * PA6 = MISO
+	 * PA7 = MOSI
+	 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6 | GPIO_Pin_5;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	// connect SPI1 pins to SPI alternate function
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+	
+	// enable clock for used IO pins
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	
+	/* Configure the chip select pin
+	   in this case we will use PA4 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	GPIOA->BSRRL |= GPIO_Pin_4; // set PA4 high
+#if 0
+	/* Configure the chip select pin
+	   in this case we will use PE7 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOE, &GPIO_InitStruct);
+	
+	GPIOE->BSRRL |= GPIO_Pin_7; // set PE7 high
+#endif	
+	// enable peripheral clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+	
+	/* configure SPI1 in Mode 0 
+	 * CPOL = 0 --> clock is low when idle
+	 * CPHA = 0 --> data is sampled at the first edge
+	 */
+	SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // set to full duplex mode, seperate MOSI and MISO lines
+	SPI_InitStruct.SPI_Mode = SPI_Mode_Master;     // transmit in master mode, NSS pin has to be always high
+	SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // one packet of data is 8 bits wide
+	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low;        // clock is low when idle
+	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge;      // data sampled at first edge
+	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set; // set the NSS management to internal and pull internal NSS high
+	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; // SPI frequency is APB2 frequency / 4
+	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;// data is transmitted MSB first
+	SPI_Init(SPI1, &SPI_InitStruct); 
+	
+	SPI_Cmd(SPI1, ENABLE); // enable SPI1
+}
+
+uint8_t SPI1_send(uint8_t data){
+
+	SPI1->DR = data; // write data to be transmitted to the SPI data register
+	while( !(SPI1->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+	while( !(SPI1->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+	while( SPI1->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
+	return SPI1->DR; // return received data from SPI data register
+}
 
 /**
  * @brief  Main program.
@@ -670,6 +749,22 @@ int main(void)
 {
 #ifdef SET_CPU_CLOCK
   SystemInit();
+#endif
+  init_SPI1();
+  uint8_t received_val = 0;
+
+#if 1
+  while(1)
+  {
+    //GPIOE->BSRRH |= GPIO_Pin_7; // set PE7 (CS) low
+    GPIOA->BSRRH |= GPIO_Pin_4; // set PE7 (CS) low
+    #if 1
+    SPI1_send(0xAA);  // transmit data
+    received_val = SPI1_send(0x12); // transmit dummy byte and receive data
+    #endif
+    //GPIOE->BSRRL |= GPIO_Pin_7; // set PE7 (CS) high
+    GPIOA->BSRRL |= GPIO_Pin_4; // set PE7 (CS) high
+  }
 #endif
 
 #if 0
@@ -703,6 +798,7 @@ int main(void)
   ur_puts(USART2, "\r\n");
 #endif
 
+#if 0
   // init SPI 1, spi1 use PA15, PB3, PB4, PB5
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB|RCC_AHB1Periph_GPIOA, ENABLE);	
 
@@ -721,7 +817,7 @@ int main(void)
   GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_SPI1);
 
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, DISABLE);
+  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, DISABLE);
 
   SPI_InitTypeDef spi_init;
 
@@ -737,8 +833,9 @@ int main(void)
 
   SPI_Init(SPI1, &spi_init);
   SPI_Cmd(SPI1, ENABLE);
+  SPI1->I2SCFGR &= (~(1<11));
   spi1_rw_byte(0xff);
-
+#endif
   while(1)
   {
     //delay(0x3FFFFF);
