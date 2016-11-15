b24091fd71314abd9a95237237e2a27c0e8f8dcb
descent <descent0@gmail.com>
2016-11-15 18:37:03 +0800

    add cross compile option

--- load_from_sd/rule.mk
+++ load_from_sd/rule.mk
@@ -1,4 +1,10 @@
-CC=gcc
-CXX=g++
-CFLAGS=-g 
+#AR=ar
+#CC=gcc
+#CXX=g++
+
+CROSS=arm-none-eabi-
+AR=$(CROSS)ar
+CC=$(CROSS)gcc
+CXX=$(CROSS)g++
+CFLAGS=-g -DSTM32F407 -std=c99 -nostartfiles -fno-common -O0 -g -mcpu=cortex-m3 -mthumb 
 
