428d1cf4cbd6db6cff329e1ade9a2e7b9f790f3e
descent <descent0@gmail.com>
2016-11-16 14:32:57 +0800

    add ARCH cm3

--- load_from_sd/rule.mk
+++ load_from_sd/rule.mk
@@ -1,10 +1,16 @@
+CFLAGS=-g -std=c99 
+
+ifeq ($(ARCH), cm3) 
+CROSS=arm-none-eabi-
+CFLAGS +=-DSTM32F407 -nostartfiles -fno-common -O0 -g -mcpu=cortex-m3 -mthumb 
+else
+endif
+
 #AR=ar
 #CC=gcc
 #CXX=g++
 
-CROSS=arm-none-eabi-
 AR=$(CROSS)ar
 CC=$(CROSS)gcc
 CXX=$(CROSS)g++
-CFLAGS=-g -DSTM32F407 -std=c99 -nostartfiles -fno-common -O0 -g -mcpu=cortex-m3 -mthumb 
 
